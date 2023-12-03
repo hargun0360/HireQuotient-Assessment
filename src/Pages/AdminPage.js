2218dbb06e38ebab1c21862b94eb7d7b75acc54c
hargun0360 <hargunsingh0720@gmail.com>
2023-12-03 22:40:18 +0530

    Add: Handle Delete and Select multiple Rows Logic

--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import useFetchData from "../Hooks/useFetchData";
-import { FETCH_DATA_URL } from "../Constant";
+import React , {useEffect , useState} from 'react'
+import useFetchData from '../Hooks/useFetchData'
+import { FETCH_DATA_URL } from '../Constant';
 import {
   DataGrid,
   GridRowModes,
@@ -18,17 +18,20 @@ import {
   DropdownMenu,
   DropdownToggle,
   UncontrolledDropdown,
-} from "reactstrap";
-import { columnHeading } from "../Constant";
-import DeleteIcon from "@mui/icons-material/Delete";
-import ModeSharpIcon from "@mui/icons-material/ModeSharp";
+} from "reactstrap"
+import { columnHeading } from '../Constant';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeSharpIcon from '@mui/icons-material/ModeSharp';
+
 
 const AdminPage = () => {
   const data = useFetchData(FETCH_DATA_URL);
   const [fetchData, setFetchData] = useState([]);
   const [input, setInput] = useState("");
   const [rowSelectionModel, setRowSelectionModel] = useState([]);
-
+  const [rowModesModel, setRowModesModel] = useState({});
+  
+  
   useEffect(() => {
     if (data) {
       setFetchData(data);
@@ -45,62 +48,170 @@ const AdminPage = () => {
     setFetchData(filteredRows);
   };
 
-  return (
-    <>
-      <div className="datatable">
-        <div className="datatableTitle">
-          {/* <SearchBar searchHandler={searchHandler} /> */}
-          <button className="button">
-            <DeleteIcon />
-          </button>
-        </div>
-        <DataGrid
-          className="datagrid"
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 10 },
-            },
-          }}
-          columns={columnHeading || []}
-          // slots={{
-          //   pagination: CustomPagination,
-          // }}
-          
-          sx={{
-            "&.MuiDataGrid-root .MuiDataGrid-cell:focus-within": {
-              outline: "none !important",
-            },
-            "&.MuiDataGrid-root .MuiDataGrid-columnHeader:focus-within": {
-              outline: "none",
-            },
-          }}
-          checkboxSelection
-          getRowClassName={(params) => `fade-in-row`}
-          showCellVerticalBorder
-          showColumnVerticalBorder
-          autoHeight
-          rows={fetchData.filter((user) => {
-            if (input === "") return user;
-            return (
-              user.name.toLowerCase().includes(input.toLowerCase()) ||
-              user.email.toLowerCase().includes(input.toLowerCase()) ||
-              user.role.toLowerCase().includes(input.toLowerCase())
-            );
-          })}
-          // columns={columns.concat(actionsColumn)}
-          pageSizeOptions={[10]}
-          editMode="row"
-          // rowModesModel={rowModesModel}
-          // onRowModesModelChange={handleRowModesModelChange}
-          // onRowEditStop={handleRowEditStop}
-          // processRowUpdate={processRowUpdate}
-          onRowSelectionModelChange={(newRowSelectionModel) => {
-            setRowSelectionModel(newRowSelectionModel);
-          }}
-        />
-      </div>
-    </>
-  );
-};
+  const actionsColumn = [
+    {
+      field: "actions",
+      headerName: "Actions",
+      type: "actions",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      renderCell: (params) => {
+        const isInEditMode =
+          rowModesModel[params.id]?.mode === GridRowModes.Edit;
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              key="save"
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: "primary.main",
+              }}
+              onClick={handleSaveClick(params.id)}
+            />,
+            <GridActionsCellItem
+              key="cancel"
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(params.id)}
+              color="inherit"
+            />,
+          ];
+        }
+        return [
+          <GridActionsCellItem
+            key="edit"
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(params.id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            key="delete"
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(params.id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
 
-export default AdminPage;
+  
+
+
+  const searchHandler = (userInput) => {
+    setInput(userInput);
+  };
+
+  const deleteHandler = () => {
+    handleDeleteMultiple(rowSelectionModel);
+  };
+
+  const handleRowEditStop = (params, event) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id) => () => {
+    handleDeleteSingle(id);
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+
+  return (<>
+    <div className="datatable">
+      <div className="datatableTitle">
+        <SearchBar searchHandler={searchHandler} />
+        <button onClick={deleteHandler} className="button">
+          <DeleteIcon />
+        </button>
+      </div>
+      <DataGrid
+        className="datagrid"
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 10 },
+          },
+        }}
+        slots={{
+          pagination: CustomPagination,
+        }}
+        components={{
+          NoRowsOverlay: () => (
+            <Stack height="100%" alignItems="center" justifyContent="center">
+              No User Found !!
+            </Stack>
+          ),
+        }}
+        sx={{
+          "&.MuiDataGrid-root .MuiDataGrid-cell:focus-within": {
+            outline: "none !important",
+          },
+          "&.MuiDataGrid-root .MuiDataGrid-columnHeader:focus-within": {
+            outline: "none",
+          },
+        }}
+        checkboxSelection
+        getRowClassName={(params) => `fade-in-row`}
+        showCellVerticalBorder
+        showColumnVerticalBorder
+        autoHeight
+        rows={rows.filter((user) => {
+          if (input === "") return user;
+          return (
+            user.name.toLowerCase().includes(input.toLowerCase()) ||
+            user.email.toLowerCase().includes(input.toLowerCase()) ||
+            user.role.toLowerCase().includes(input.toLowerCase())
+          );
+        })}
+        columns={columns.concat(actionsColumn)}
+        pageSizeOptions={[10]}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        onRowSelectionModelChange={(newRowSelectionModel) => {
+          setRowSelectionModel(newRowSelectionModel);
+        }}
+      />
+    </div>
+  </>);
+}
+
+export default AdminPage
